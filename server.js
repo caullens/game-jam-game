9ce9523ca8a4d28f12b01c251e351fa6840b6a37
d526f861 <d526f861@gmail.com>
2017-09-15 21:13:18 -0500

    server change

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var app = express()
 
 app.use('/', (req, res) => {
   var urlParts = req.url.split('/')
-  console.log(urlParts)
-  if(urlParts[1] === '') res.sendFile(path.join(__dirname + '/public/index.html'))
+  if(urlParts[1] === '' || urlParts[1] === ':1') res.sendFile(path.join(__dirname + '/public/index.html'))
   if(urlParts[1] === 'assets') res.sendFile(path.join(__dirname + '/assets/'+urlParts[2]))
   else res.sendFile(path.join(__dirname + '/public/'+urlParts[1]))
 })
