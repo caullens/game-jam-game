1cfa35353e7ff43c670d8ab6aa7dea85b7183ddb
Caullen Sasnett <caullens@ksu.edu>
2017-09-15 21:23:55 -0500

    Added button class

--- public/sketch.js
+++ public/sketch.js
@@ -3,25 +3,33 @@ var state
 function setup() {
   //frameRate(5);
   state = 'menu'
-  maze = new Maze(100)
+  maze = new Maze(200)
   maze.setup()
   createCanvas(600, 600);
+  easyButton = new Button({x: 10, y: 550}, {width: 150, height: 40}, 'game', "Easy", ['menu'])
+  mediumButton = new Button({x: 225, y: 550}, {width: 150, height: 40}, 'game', "Medium", ['menu'])
+  hardButton = new Button({x: 440, y: 550}, {width: 150, height: 40}, 'game', "Hard", ['menu'])
 }
 
 function draw() {
+  background(51)
+  easyButton.update(state)
+  mediumButton.update(state)
+  hardButton.update(state)
+
+  easyButton.render()
+  mediumButton.render()
+  hardButton.render()
+
   if(state === 'menu') {
-    background(51)
-    color('black');
-    rect(250, 500, 100, 40);
-    textAlign(CENTER);
-    textFont('Verdana', 30);
-    color('white');
-    text("Start", 300, 535);
   } else if (state === 'game') {
     maze.draw();
   }
 }
 
 function mouseClicked() {
-  if((mouseX > 250 && mouseX < 350) && (mouseY > 500 && mouseY < 540)) state = "game";
+  console.log("Mouse clicked")
+  if(easyButton.isClicked(mouseX, mouseY)) state = easyButton.state
+  if(mediumButton.isClicked(mouseX, mouseY)) state = mediumButton.state
+  if(hardButton.isClicked(mouseX, mouseY)) state = hardButton.state
 }
--- server.js
+++ server.js
@@ -5,9 +5,8 @@ var app = express()
 
 app.use('/', (req, res) => {
   var urlParts = req.url.split('/')
-  console.log(urlParts)
-  if(urlParts[1] === '') res.sendFile(path.join(__dirname + '/public/index.html'))
-  if(urlParts[1] === 'assets') res.sendFile(path.join(__dirname + '/assets/'+urlParts[2]))
+  if(urlParts[1] === '' || urlParts[1] === ':1') res.sendFile(path.join(__dirname + '/public/index.html'))
+  else if(urlParts[1] === 'assets') res.sendFile(path.join(__dirname + '/assets/'+urlParts[2]))
   else res.sendFile(path.join(__dirname + '/public/'+urlParts[1]))
 })
 
